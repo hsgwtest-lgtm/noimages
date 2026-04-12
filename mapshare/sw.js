a7bd11379a0b56ef8edd1ed4788c6d1d56b7ec9e
hsgwtest-lgtm <eiichitiktok@gmail.com>
2026-04-12 20:48:38 +0900

    Add MapShare - リアルタイム位置情報共有PWAアプリ

---
+++ mapshare/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'mapshare-v1';
+const ASSETS = [
+  '/noimages/mapshare/',
+  '/noimages/mapshare/index.html',
+  '/noimages/mapshare/manifest.json',
+  '/noimages/mapshare/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
